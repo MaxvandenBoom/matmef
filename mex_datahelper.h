56e805f1f7e1c9878a4fa82c802b037d00d94d00
Max <m.a.vandenboom84@gmail.com>
2020-01-19 22:19:10 -0600

    removed unused functions

--- mex_datahelper.h
+++ mex_datahelper.h
@@ -14,9 +14,6 @@ mxArray *mxUInt32ByValue(ui4 value);
 mxArray *mxInt32ByValue(si4 value);
 mxArray *mxInt64ByValue(si8 value);
 
-//mxArray *mxDoubleByValue(double value);
-//mxArray *mxIntByValue(int value, bool sign, int bits);
-
 
 
 #endif   // MEX_DATAHELPER_
