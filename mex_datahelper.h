337b72cd4373167fbc9e6c0cc9b1ee0e3bfdfbef
Max <m.a.vandenboom84@gmail.com>
2020-04-14 16:03:24 -0500

    Added comment

--- mex_datahelper.h
+++ mex_datahelper.h
@@ -1,10 +1,18 @@
 #ifndef MEX_DATAHELPER_
 #define MEX_DATAHELPER_
 /**
+ * 	@file - headers
+ * 	Functions to convert primitive c-datatypes to matlab primitive (1x1) arrays/matrices
+ *	
+ *  Copyright 2020, Max van den Boom
+ *  Includes updates from Richard J. Cui - richard.cui@utoronto.ca (4 apr 2020)
  *
- * 	@file - header
- *
- *
+ *  
+ *  This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+ *  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *  You should have received a copy of the GNU General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 #include "mex.h"
 #include "meflib/meflib/meflib.h"
