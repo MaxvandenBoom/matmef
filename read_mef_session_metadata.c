6e729b895ac6d16509346d1310fed576db8f8a6c
MaxvandenBoom <m.a.vandenboom84@gmail.com>
2021-11-27 23:43:36 -0600

    Added missing include for mac compiling

--- read_mef_session_metadata.c
+++ read_mef_session_metadata.c
@@ -14,6 +14,7 @@
  *  You should have received a copy of the GNU General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 #include "mex.h"
+#include "matmef_dataconverter.h"
 #include "matmef_mapping.h"
 
 #include "meflib/meflib/meflib.c"
